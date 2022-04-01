fcf8a893d500ea2f67f44eb16f4d2f5818df8672
Yusuf <bsse1013@iit.du.ac.bd>
2022-04-01 07:25:56 +0600

    q3

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
+import random
+
 credentials = {}
 salt = "adsf"
+search_str = "abcdefghijklmnopqrstuvwxyz"
 
 
 def hash(in_str):
-    search_str = "abcdefghijklmnopqrstuvwxyz"
     sum = 0
     for char in in_str:
         sum += search_str.find(char) + 1
 
     return sum
+
+
+def match_password(password_input, hash_value):
+    for char in search_str:
+        if hash_value == hash(password_input+salt+char):
+            return True
+
+    return False
 
 
 def main():
@@ -19,14 +29,16 @@ def main():
             password = input("password: ")
             if username not in credentials:
                 print("User does not exist")
-            elif credentials[username] == hash(password+salt):
+            elif match_password(password, credentials[username]):
                 print("success")
             else:
                 print("failed")
         elif choice == "2":
             username = input("username: ")
             password = input("password: ")
-            credentials[username] = hash(password+salt)
+            pepper = random.choice(search_str)
+            print(pepper)
+            credentials[username] = hash(password+salt+pepper)
         else:
             print("error")
 
