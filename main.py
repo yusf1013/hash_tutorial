12c3384d49a6b323caf7286c4ddb6c945913cced
Yusuf <bsse1013@iit.du.ac.bd>
2022-04-04 11:58:01 +0600

    question 25

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import random, bcrypt
+import time
+from itertools import permutations
 
 credentials = {}
 salt = bcrypt.gensalt()
-search_str = "abcdefghijklmnopqrstuvwxyz"
-# s = bcrypt.gensalt()
 
 
-def hash(in_str):
-    sum = 0
-    for char in in_str:
-        sum += search_str.find(char) + 1
+def generate(starting_str, length):
+    ans = []
+    perms = permutations(starting_str, length)
+    for k in list(perms):
+        item = ""
+        for x in k:
+            item += x
+        ans.append(item)
 
-    return sum
-
-
-def match_password(password_input, hash_value):
-    for char in search_str:
-        if hash_value == hash(password_input+salt+char):
-            return True
-
-    return False
+    return ans
 
 
 def main():
     while True:
-        choice = input("1. Login\n2. Register\n")
+        choice = input("1. Login\n2. Register\n3. Bruteforce\n")
         if choice == "1":
             username = input("username: ")
             password = input("password: ")
@@ -42,6 +38,22 @@ def main():
             # hashed = bcrypt.hashpw(bytes(password, encoding='utf-8'), s)
             # credentials[username] = hash(password+salt+pepper)
             credentials[username] = bcrypt.hashpw(bytes(password, encoding='utf-8'), salt)
+        elif choice == "3":
+            username = input("enter victim's username: ")
+            p1 = generate("0123456789", 2)
+            # p1 = generate("abcdefghijklmnopqrstuvwxyz", 4)
+            # p1 = generate("abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ", 3)
+            start = time.time()
+            for password in p1:
+                if username not in credentials:
+                    print("User does not exist")
+                elif bcrypt.checkpw(bytes(password, encoding='utf-8'), credentials[username]):
+                    print("success", password)
+                    end = time.time()
+                    print(end - start)
+                    break
+                else:
+                    print("failed", password)
         else:
             print("error")
 
